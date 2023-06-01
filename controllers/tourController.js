e52936149d29cc4bb2029d0b936e0f33cecb7209
hbpphuc <100748345+hbpphuc@users.noreply.github.com>
2023-06-01 13:30:48 +0700

    aggregation pipeline matching and grouping

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -80,3 +80,30 @@ exports.deleteTour = catchAsync(async (req, res, next) => {
         data: null,
     });
 });
+
+exports.getTourStats = catchAsync(async (req, res, next) => {
+    const stats = await Tour.aggregate([
+        {
+            $match: { ratingsAverage: { $gte: 4.5 } },
+        },
+        {
+            $group: {
+                _id: '$difficulty',
+                numTour: { $sum: 1 },
+                numRating: { $sum: '$ratingsQuantity' },
+                avgRating: { $sum: '$ratingsAverage' },
+                avgPrice: { $avg: '$price' },
+                minPrice: { $min: '$price' },
+                maxPrice: { $max: '$price' },
+            },
+        },
+        {
+            $sort: { avgPrice: 1 },
+        },
+    ]);
+
+    res.status(200).json({
+        status: 'success',
+        data: { stats },
+    });
+});
