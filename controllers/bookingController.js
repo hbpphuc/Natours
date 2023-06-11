1b7950e45df45aa835abfd662fca30af98a35a13
hbpphuc <100748345+hbpphuc@users.noreply.github.com>
2023-06-11 17:34:17 +0700

    coding payment with stripe webook

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -18,7 +18,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
         payment_method_types: ['card'],
         success_url: `${req.protocol}://${req.get('host')}/?tour=${
             req.params.tourId
-        }&user=${req.user.id}&price=${tour.price}`,
+        }&user=${req.user.id}&price=${tour.price}&alert=booking`,
+        // success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
         cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
         customer_email: req.user.email,
         client_reference_id: req.params.tourId,
@@ -58,6 +59,33 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
 
     res.redirect(req.originalUrl.split('?')[0]);
 });
+
+// const createBookingCheckout = async (session) => {
+//     const tour = session.client_reference_id;
+//     const user = (await User.findOne({ email: session.customer_email })).id;
+//     const price = session.line_items[0].price_data.unit_amount / 100;
+//     await Booking.create({ tour, user, price });
+// };
+
+// exports.webhookCheckout = (req, res, next) => {
+//     const signature = req.headers['stripe-signature'];
+
+//     let event;
+//     try {
+//         event = stripe.webhooks.constructEvent(
+//             req.body,
+//             signature,
+//             process.env.STRIPE_WEBHOOK_SECRET
+//         );
+//     } catch (error) {
+//         return res.status(400).send(`Webhook error: ${error.message}`);
+//     }
+
+//     if (event.type === 'checkout.session.completed')
+//         createBookingCheckout(event.data.object);
+
+//     res.status(200).json({ received: true });
+// };
 
 exports.getAllBookings = crudHandler.getAll(Booking);
 exports.createBooking = crudHandler.createOne(Booking);
